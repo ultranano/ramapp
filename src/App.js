3d2862d1d49d7f64a6f4cce8c8ef83a647049b04
Andrea Baldon <andrea.baldon@axa.it>
2022-02-14 11:33:04 +0100

    new test logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './ramlogo.jpg';
+import logo from './ramlogo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="ram logo" />
-        <p>
-          Rick & Morty v 1.0
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
